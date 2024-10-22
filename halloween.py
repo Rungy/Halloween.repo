ab1aec79d07cb455979823a334978914c7fc1ddb
Otto karlsson <otto.karlsson@elev.ga.ntig.se>
2024-10-22 10:32:16 +0200

    halloween.py

--- halloween.py
+++ halloween.py
@@ -1,6 +1,6 @@
 # halloween.py
 
-# Fråga användaren om deras favorit-Halloween-karaktär
+# Fråga användaren om deras favorit halloween karaktär
 karaktar = input("Vilken är din favorit-Halloween-karaktär? (vampyr, zombie, spöke): ").lower()
 
 # Använd en if-sats för att ge ett svar baserat på användarens val
@@ -10,13 +10,28 @@ elif karaktar == "zombie":
     print("Hjärnor... hjärnor...")
 elif karaktar == "spöke":
     print("Buuuu!")
-elif karaktar == "Varulv":
+elif karaktar == "varulv":
     print("Månen är på din sida!")
 elif karaktar == "Mummy":
     print("Määäääh")
-elif karaktar == "Troll":
+elif karaktar == "troll":
     print("Troll jävel.")
-elif karaktar == "Trollkarl":
+elif karaktar == "trollkarl":
     print("Wow Magi!")
 else:
     print("Det låter som en spännande karaktär!")
+
+#Fråga användaren om deras favorit godis
+godis = input("Vad är din favorit godis?").lower()
+
+#ännu en if-sats för att ge ett svar baserat på användarens val
+if godis == "twix":
+    print("Såja så ska de låta")
+elif godis == "snicker":
+    print("Bra men it bäst.")
+else:
+    x = 0
+    while x < 5:
+        x = x + 1
+        print("Bläh!")
+
